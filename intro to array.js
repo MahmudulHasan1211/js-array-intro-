a2a257bee75e098ca33449687cb8e6e273e8ed66
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 00:53:53 +0600

    put array index value into another variable

--- intro to array.js
+++ intro to array.js
@@ -4,4 +4,8 @@ console.log(numbers.length);
 
 // index of array 
 const number = [23, 33, 44, 55, 66, 77, 88, 99]
-console.log(number[4])
+console.log(number[10])
+
+// set index value in another variable 
+const niw = number[7];
+console.log(niw);
