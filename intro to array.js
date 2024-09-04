88591df0841f1d899dd0e993aa355642f3dad12b
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 01:23:25 +0600

    push pop - put push value into new variable added

--- intro to array.js
+++ intro to array.js
@@ -13,3 +13,18 @@ console.log(niw);
 // set or update element value by index names 
 number[0] = 172;
 console.log(number)
+
+// push new items into array 
+let hasa = [10, 20, 30, 40, 50,]
+console.log(hasa)
+hasa.push(60)
+console.log(hasa)
+
+// pop items from array 
+hasa.pop()
+console.log(hasa)
+//  out value from another array and put into new variable
+const tik = hasa.pop()
+console.log(tik)
+
+
