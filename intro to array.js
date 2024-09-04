53d6ba09446e1c8b5c0e6239024dcb0ef47124f6
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 00:47:44 +0600

    value of index added

--- intro to array.js
+++ intro to array.js
@@ -1,2 +1,7 @@
+//calculate the length of numbers array
 const numbers = [23, 33, 44, 55, 66, 77, 88, 99]
 console.log(numbers.length);
+
+// index of array 
+const number = [23, 33, 44, 55, 66, 77, 88, 99]
+console.log(number[4])
