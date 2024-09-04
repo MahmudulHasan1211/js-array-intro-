f5fb92862575f50a68059b97321cd5f7de6d50ad
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-05 01:01:48 +0600

    set or update element value by index names

--- intro to array.js
+++ intro to array.js
@@ -4,8 +4,12 @@ console.log(numbers.length);
 
 // index of array 
 const number = [23, 33, 44, 55, 66, 77, 88, 99]
-console.log(number[10])
+console.log(number[0])
 
 // set index value in another variable 
 const niw = number[7];
 console.log(niw);
+
+// set or update element value by index names 
+number[0] = 172;
+console.log(number)
